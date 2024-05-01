e795e0b4a5407a96f47dd28daa23d9b20b65d9fe
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-05-01 18:50:14 +0000

    home is good

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -1,6 +1,22 @@
 class BasicController < ApplicationController
 
-  def show
-  render({:template => "home"})
+  def home
+    render({:template => "home"})
+  end 
+
+ def photo
+     render({:template => "photo"})
+    end 
+
+ def wood
+    render({:template => "wood"})
+  end 
+
+ def about
+        render({:template => "about"})
+        end 
+
+  def other
+     render({:template => "other"})
   end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get("/", {:controller => "basic", :action=> "show"})
+  get("/", {:controller => "basic", :action=> "home"})
 
-  get("/about", {:controller => "basic", :action=> "show"})
+  get("/about", {:controller => "basic", :action=> "about"})
 
-  get("/photography", {:controller=> "basic", :action= "show"})
+  get("/photography", {:controller=> "basic", :action=> "photo"})
 
-  get("/wood", {:controller=> "basic", :action= "show"})
+  get("/furniture", {:controller=> "basic", :action=> "wood"})
 
-  get("/other", {:controller=> "basic", :action= "show"})
+  get("/other", {:controller=> "basic", :action=> "other"})
 
   # Defines the root path route ("/")
   # root "articles#index"
